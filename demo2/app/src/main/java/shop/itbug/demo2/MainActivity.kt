ebecb5cc1409ea4fdf21d43ac2524ea5e1df9dec
diandian <hlxsmail@gmail.com>
2020-08-19 19:35:13 +0800

    生命周期

--- demo2/app/src/main/java/shop/itbug/demo2/MainActivity.kt
+++ demo2/app/src/main/java/shop/itbug/demo2/MainActivity.kt
@@ -1,11 +1,56 @@
 package shop.itbug.demo2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val tag = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(tag,"on create...")
+        startNormalActivity.setOnClickListener{
+            val intent = Intent(this,NormalActivity::class.java)
+            startActivity(intent)
+        }
+        startDialogActivity.setOnClickListener{
+            val intent = Intent(this,DialogActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(tag,"on start...")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(tag,"on resume...")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(tag,"on pause...")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(tag,"on stop...")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(tag,"on destory...")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(tag,"on restart...")
     }
 }
---
+++ demo2/app/src/main/java/shop/itbug/demo2/NormalActivity.kt
@@ -0,0 +1,11 @@
+package shop.itbug.demo2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NormalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_normal)
+    }
+}
