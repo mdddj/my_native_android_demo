d5de9e1f9d7c5ba96b37d5fa0f443bfa75fbf3e4
diandian <hlxsmail@gmail.com>
2020-08-20 18:51:00 +0800

    修改

--- demo4/app/src/main/java/shop/itbug/demo4/MainActivity.kt
+++ demo4/app/src/main/java/shop/itbug/demo4/MainActivity.kt
@@ -1,11 +1,58 @@
 package shop.itbug.demo4
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.*
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val inputText = load()
+        if(inputText.isNotEmpty()){
+            inputEdit.setText(inputText)
+            inputEdit.setSelection(inputText.length)
+            Toast.makeText(this,"加载本地文件成功",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        val inputText = inputEdit.text.toString()
+        save(inputText)
+        Log.d("MainActivity", "程序销毁")
+    }
+
+    private fun save(inputText: String) {
+        try {
+            val output = openFileOutput("data", Context.MODE_PRIVATE)
+            val white = BufferedWriter(OutputStreamWriter(output))
+            white.use {
+                it.write(inputText)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun load(): String {
+        val content = StringBuilder()
+        try {
+            val input = openFileInput("data")
+            val reader = BufferedReader(InputStreamReader(input))
+            reader.use {
+                reader.forEachLine {
+                    content.append(it)
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return content.toString()
     }
 }
